8ca04f81e303ff31c34b3ab99c18e5597556b6b9
Sergei Afonin <s.afonin@bankffin.ru>
2022-12-28 21:06:40 +0300

    add spring-rabbit-producer and spring-rabbit-consumer

--- src/main/java/com/example/springcodeexamples/from_console_to_web/ConsoleProcessorApp.java
+++ src/main/java/com/example/springcodeexamples/from_console_to_web/ConsoleProcessorApp.java
@@ -1,27 +1,24 @@
-//package com.example.springcodeexamples.from_console_to_web;
-//
-//import com.rabbitmq.client.Channel;
-//import com.rabbitmq.client.Connection;
-//import com.rabbitmq.client.ConnectionFactory;
-//import com.rabbitmq.client.DeliverCallback;
-//import lombok.extern.slf4j.Slf4j;
-//
-//@Slf4j
-//public class ConsoleProcessorApp {
-//    public static final String QUEUE_FOR_PROCESSING_NAME = "DISPOSAL2_CREATE_BATCH_EXCHANGE";
-//    public static final String EXCHANGER_FOR_PROCESSING_RESULTS = "disposal_v2";
-//
-//    public static void main(String[] argv) throws Exception {
-//        ConnectionFactory factory = new ConnectionFactory();
-//        factory.setHost("localhost");
-//        factory.setPort(9090);
-//        Connection connection = factory.newConnection();
-//        Channel channel = connection.createChannel();
-//
-//
-//        for (int i = 0; i < 10; i++) {
-//            log.info("Сообщение отправлено");
-//            channel.basicPublish(EXCHANGER_FOR_PROCESSING_RESULTS, "sms", null, "Task ready".getBytes());
-//        }
-//    }
-//}
+package com.example.springcodeexamples.from_console_to_web;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConsoleProcessorApp {
+    public static final String EXCHANGER_FOR_PROCESSING_RESULTS = "disposal_v2";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(9090);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        for (int i = 0; i < 10; i++) {
+            log.info("Сообщение отправлено");
+            channel.basicPublish(EXCHANGER_FOR_PROCESSING_RESULTS, "sms", null, "Task ready".getBytes());
+        }
+    }
+}
