ca452cdf66263654a989ce4ac69ad1bfe914ab42
Sergei Afonin <s.afonin@bankffin.ru>
2022-12-28 14:04:28 +0300

    First commit

---
+++ src/main/java/com/example/springcodeexamples/SpringCodeExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcodeexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCodeExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCodeExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcodeexamples/SpringCodeExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcodeexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodeExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
